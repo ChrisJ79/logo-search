a3f762dd63e2f6c07640ec359499306d15c565db
Chris Johnson <csjohnson79@icloud.com>
2016-07-10 20:41:07 -0600

    continued work on html & css

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-$
+
+
+var 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+-----JQUERY GUIDE FROM QUIZ-APP-----
+    $('.end-quiz').on('click', '.try-again-button', function () {
+        $('.start-quiz').show();
+        $('.quiz-questions').hide();
+        $('.end-quiz').hide();
+	    // Reset variables to start quiz again.
+	    currentQuestionNumber = 0;
+	    numberOfCorrectAnswers = 0;
+	});
+
+
+*/
